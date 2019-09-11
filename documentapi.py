18a402d45da184a3311793ddfaab00181c33966a
Matthew Reeve <matthew.reeve@slalom.com>
2019-09-11 14:28:00 +0100

    Adding DocumentAPI

--- documentapi.py
+++ documentapi.py
@@ -1,7 +1,27 @@
+from tableaudocumentapi import Workbook
 import logging
 
 logger = logging.getLogger(__name__)
 
 
 def initial(filename):
-    return 1
+    wb = Workbook(filename)
+    data = []
+
+    for datasource in wb.datasources:
+        fields = []
+        for field in datasource.fields.values():
+            fields.append({
+                'Name': field.name,
+                'Role': field.role,
+                'Type': field.datatype,
+                # 'Number Type': field.type,
+                'Caption': field.caption,
+                'Calculation': field.calculation == '1',
+            })
+        data.append({
+            'name': datasource.name,
+            'caption': datasource.caption,
+            'fields': fields
+        })
+    return data
